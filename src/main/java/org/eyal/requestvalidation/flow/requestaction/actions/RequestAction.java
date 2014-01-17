9e5a1beadf3f209864d76d378674d1ad4498aed1
Eyal Golan <egolan@ebay.com>
2014-01-18 01:44:02 +0200

    remove action package. will not use it

--- src/main/java/org/eyal/requestvalidation/flow/requestaction/actions/RequestAction.java
+++
@@ -1,7 +0,0 @@
-package org.eyal.requestvalidation.flow.requestaction.actions;
-
-import org.eyal.requestvalidation.model.Request;
-
-public interface RequestAction {
-	Request apply(Request maiaInsertRequest);
-}
